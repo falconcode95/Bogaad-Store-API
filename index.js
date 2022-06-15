b2197cb32d07e90b0855f526514ddfea9c1e603e
Abdullaziz <a.z.mohamed@hotmail.com>
2022-06-15 04:29:49 +0300

    same forgot which commit number

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 const Pool = require('pg').Pool;
 const cors = require('cors');
-const { response } = require('express');
+const CryptoJS = require('crypto-js');
+// const { response } = require('express');
+const jwt = require('jsonwebtoken');
 const express = require('express');
-const app = express()
-const port = 5000
-let result;
+require('dotenv').config();
+const app = express();
+const stripe = require('stripe')(process.env.STRIPE_KEY)
 const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  port: 5432,
-  password: 'postgres',
-  database: 'Bogaad Store'
+  host: process.env.HOST,
+  user: process.env.USER,
+  port: process.env.db_PORT,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 })
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+
+app.post('/payment', async (req, res)=> {
+  try {  
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: 'payment',
+      line_items: req.body,
+      success_url: 'http://localhost:3000/success',
+      cancel_url: 'http://localhost:3000/Cart'
+    })
+    console.log(session.url)
+    res.json({url: session.url})
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 app.get('/products/:category/:id', async(req,res) => {
   try {
     const urlParams = req.params
-    const query = await pool.query("SELECT * FROM " + urlParams.category +  " WHERE id = " + parseInt(urlParams.id));
+    const data = parseInt(urlParams.id);
+    const text = 'SELECT * FROM ' + urlParams.category + ' WHERE id = $1'
+    const values = [data]
+    const query = await pool.query(text, values);
     const b64 = query.rows[0].image
     res.setHeader('Content-type', 'image/jpg');
     res.send(b64);
@@ -28,19 +49,49 @@ app.get('/products/:category/:id', async(req,res) => {
   }
 })
 
-app.get('/users', async(req,res) => {
+app.get('/shortcut', (req, res)=> {
   try {
-    const object = req.headers;
-    // console.log(object)
-    let query = await pool.query(`SELECT * FROM users WHERE email = '${object.email}';`);
-    const dbResponse = query.rows[0];
-    if(dbResponse.password === object.password){
+      const token = req.headers.accesstoken.split(' ')[1];
+      jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, userCredentials)=> {
+        if(err){
+          res.status(401)
+      }
+      let query = await pool.query(`SELECT * FROM users WHERE email = '${userCredentials.email}';`);
+      const dbResponse = query.rows[0];
       query = await pool.query(`SELECT * FROM user_data WHERE user_id = '${dbResponse.id}';`);
       const normalQuery = query.rows[0];
       delete dbResponse.password;
-      dbResponse.jsonCart = normalQuery.cart_data;
-      dbResponse.jsonWishList = normalQuery.wishlist_data;
-      console.log(dbResponse)
+      if(normalQuery){
+        dbResponse.jsonCart = normalQuery.cart_data;
+        dbResponse.jsonWishList = normalQuery.wishlist_data;
+      }
+      res.status(200).send(JSON.stringify(dbResponse));
+    })
+  } catch (e) {
+    res.status(500)
+  }
+})
+
+app.get('/users', async(req,res) => {
+  try {
+    const object = req.headers;
+    const text = 'SELECT * FROM users WHERE email = $1';
+    const value = [object.email]
+    let query = await pool.query(text, value);
+    const dbResponse = query.rows[0];
+    const decryptedPassword = CryptoJS.AES.decrypt(dbResponse.password, process.env.ENCRYPT_SECRET).toString(CryptoJS.enc.Utf8);
+    if(decryptedPassword === object.password){
+      query = await pool.query(`SELECT * FROM user_data WHERE user_id = '${dbResponse.id}';`);
+      const normalQuery = query.rows[0];
+      const userCredentials = {email: object.email, password: object.password};
+      const accessToken = jwt.sign(userCredentials, process.env.JWT_SECRET_KEY, {expiresIn: '1w'})
+      // console.log(accessToken);
+      delete dbResponse.password;
+      dbResponse.accessToken = accessToken;
+      if(normalQuery){
+        dbResponse.jsonCart = normalQuery.cart_data && normalQuery.cart_data;
+        dbResponse.jsonWishList = normalQuery.wishlist_data && normalQuery.cart_data;
+      }
       res.status(200).send(JSON.stringify(dbResponse));
     } else {
       res.status(401).send()
@@ -53,10 +104,11 @@ app.get('/users', async(req,res) => {
 app.post('/users', async(req,res) => {
   try {
     const object = req.body;
-    console.log(object)
-    const query = await pool.query(`INSERT INTO users (email, name, surname, password) VALUES ( '${object.email}', '${object.name}',
-      '${object.surname}', '${object.password}') `);
-    res.status(201).send(JSON.stringify(`User with the email: ${object.email} is created`));
+    const encryptedPassword = CryptoJS.AES.encrypt(object.password, process.env.ENCRYPT_SECRET).toString();
+    const text = 'INSERT INTO users (email, name, surname, password) VALUES ($1, $2, $3, $4)'
+    const values = [object.email, object.name, object.surname, encryptedPassword]
+    const query = await pool.query(text, values);
+    res.status(201).send(JSON.stringify(`User with the email: ${object.email} is created`)); 
     } catch (e) {
       res.status(400).send()
   }
@@ -65,13 +117,17 @@ app.post('/users', async(req,res) => {
 app.put('/users', async(req,res)=> {
   try {
     const object = req.body;
-    console.log(object)
-    let query = await pool.query(`SELECT * FROM users WHERE email = '${object.email}';`);
-    // console.log(query.rows[0])
-    if(object.password === query.rows[0].password) {
-      query = await pool.query(`UPDATE users SET ${object.key} = '${object.value}'  WHERE email = '${object.email}';`);
-      query = await pool.query(`SELECT * FROM users WHERE email = '${object.email}';`);
-      console.log()
+    const text = 'SELECT * FROM users WHERE email = $1'
+    const values = [object.email]
+    let query = await pool.query(text, values);
+    const decryptedPassword = CryptoJS.AES.decrypt(query.rows[0].password, process.env.ENCRYPT_SECRET).toString(CryptoJS.enc.Utf8);
+    if(object.password === decryptedPassword) {
+      const text1 = `UPDATE users SET ${object.key} = $1  WHERE email = $2`
+      const values1 = [object.value, object.email];
+      query = await pool.query(text1, values1);
+      const text2 = 'SELECT * FROM users WHERE email = $1'
+      const values2 = [object.email];
+      query = await pool.query(text2, values2);
       res.status(200).send(JSON.stringify(query.rows[0][object.key]))
     }
 
@@ -83,11 +139,15 @@ app.put('/users', async(req,res)=> {
 app.delete('/users', async(req, res)=> {
   try {
     const object = req.body;
-    console.log(object)
-    let query = await pool.query(`SELECT * FROM users WHERE email = '${object.email}';`);
-    if(object.password === query.rows[0].password) {
-      query = await pool.query(`DELETE FROM users WHERE email = '${object.email}';`);
-      res.status(204).send(JSON.stringify({response: 'user is deleted from the database!'}))
+    const text = 'SELECT * FROM users WHERE email = $1';
+    const value = [object.email]
+    let query = await pool.query(text, value);
+    const decryptedPassword = CryptoJS.AES.decrypt(query.rows[0].password, process.env.ENCRYPT_SECRET).toString(CryptoJS.enc.Utf8);
+    if(object.password === decryptedPassword) {
+      const text1 = 'DELETE FROM users WHERE email = $1';
+      const value1 = [object.email]
+      query = await pool.query(text1, value1);
+      res.status(200).send(JSON.stringify({response: 'user is deleted from the database!'}))
     } else {
       res.status(401).send(JSON.stringify({response: 'Password is incorrect'}))
     }
@@ -99,32 +159,22 @@ app.delete('/users', async(req, res)=> {
 app.post('/userdata', async(req,res)=> {
   try {
     const object = req.body;
-
-    console.log(object);
-    // console.log(object.email, object.wishlist, object.json)
-    let query = await pool.query(`SELECT * FROM users WHERE email = '${object.email}';`);
+    const text = 'SELECT * FROM users WHERE email = $1';
+    const value = [object.email]
+    let query = await pool.query(text, value);
     const userId = query.rows[0].id;
     if(object.jsonCart && object.jsonWishList){
-    query = await pool.query(`INSERT INTO user_data (user_id, cart_data, wishlist_data) 
-          VALUES (${userId}, '${JSON.stringify(object.jsonCart)}', '${JSON.stringify(object.jsonWishList)}')
-          ON CONFLICT (user_id) 
-          DO
-          UPDATE SET cart_data = EXCLUDED.cart_data, wishlist_data = EXCLUDED.wishlist_data;`);
-          console.log('first if')
+      const text1 = 'INSERT INTO user_data (user_id, cart_data, wishlist_data) VALUES (' + userId + ' , $1, $2) ON CONFLICT (user_id) DO UPDATE SET cart_data = EXCLUDED.cart_data, wishlist_data = EXCLUDED.wishlist_data';
+      const value1 = [JSON.stringify(object.jsonCart), JSON.stringify(object.jsonWishList)]
+      let query = await pool.query(text1, value1);
     } else if(object.jsonCart && !object.jsonWishList){
-      query = await pool.query(`INSERT INTO user_data (user_id, cart_data) 
-      VALUES (${userId}, '${JSON.stringify(object.jsonCart)}')
-      ON CONFLICT (user_id) 
-      DO
-      UPDATE SET cart_data = EXCLUDED.cart_data;`);
-      console.log('second if')
+      const text2 = 'INSERT INTO user_data (user_id, cart_data) VALUES (' + userId + ' , $1) ON CONFLICT (user_id) DO UPDATE SET cart_data = EXCLUDED.cart_data, wishlist_data = EXCLUDED.wishlist_data';
+      const value2 = [JSON.stringify(object.jsonCart)]
+      let query = await pool.query(text2, value2);
     } else if(!object.jsonCart && object.jsonWishList){
-      query = await pool.query(`INSERT INTO user_data (user_id, wishlist_data) 
-      VALUES (${userId},'${JSON.stringify(object.jsonWishList)}')
-      ON CONFLICT (user_id) 
-      DO
-      UPDATE SET wishlist_data = EXCLUDED.wishlist_data;`);
-      console.log('third if')
+      const text3 = 'INSERT INTO user_data (user_id, wishlist_data) VALUES (' + userId + ' , $1) ON CONFLICT (user_id) DO UPDATE SET cart_data = EXCLUDED.cart_data, wishlist_data = EXCLUDED.wishlist_data';
+      const value3 = [JSON.stringify(object.jsonWishList)]
+      let query = await pool.query(text3, value3);
     }
     res.status(201).send(JSON.stringify({dataReceived: true}));
   } catch(e){
@@ -133,8 +183,8 @@ app.post('/userdata', async(req,res)=> {
   }
 })
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`)
 })
 
 
